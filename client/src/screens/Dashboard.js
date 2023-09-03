d4d76562884a4dd7f168a716642ee3b62f0c04f0
Abhiraj R <136077616+4bhy-tl@users.noreply.github.com>
2023-09-03 16:45:18 +0530

    project setup readme

--- client/src/screens/Dashboard.js
+++ client/src/screens/Dashboard.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchVehicle, fetchVehicles } from '../actions/adminActions'
 import { Link, useNavigate } from 'react-router-dom'
+import { loginSuccess } from '../features/loginSlice'
 
 
 const Dashboard = () => {
@@ -12,6 +13,8 @@ const Dashboard = () => {
     const [search, setSearch] = useState('')
     const { loading, vehicles, error } = useSelector(state => state.fetchVehicles)
     console.log(vehicles);
+
+    const { userInfo } = useSelector(state => state.login)
 
     const filteredVehicles = vehicles.filter((vehicle) =>
         vehicle.name.toLowerCase().includes(search.toLowerCase())
@@ -26,12 +29,22 @@ const Dashboard = () => {
         dispatch(fetchVehicles())
     }, [])
 
+    useEffect(() => {
+        if (userInfo == null) {
+            navigate('/login')
+        }
+    }, [userInfo])
+
+    const handleLogout = () => {
+        localStorage.removeItem("userInfo");
+        dispatch(loginSuccess(null))
+        navigate('/login')
+    }
 
     return (
         <>
             <div class="bg-gray-900 min-h-screen flex items-center justify-center">
                 <div class="bg-gray-800 flex-1 flex flex-col space-y-5 lg:space-y-0 lg:flex-row lg:space-x-10 max-w-6xl sm:p-6 sm:my-2 sm:mx-4 sm:rounded-2xl">
-
                     <div class="bg-gray-900 px-2 lg:px-4 py-2 lg:py-10 sm:rounded-xl flex lg:flex-col justify-between">
                         <nav class="flex items-center flex-row space-x-2 lg:space-x-0 lg:flex-col lg:space-y-2">
                             <a class="text-white/50 p-4 inline-flex justify-center rounded-md hover:bg-gray-800 hover:text-white smooth-hover" href="#">
@@ -39,30 +52,13 @@ const Dashboard = () => {
                                     <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
                                 </svg>
                             </a>
-                            {/* <!-- Active: bg-gray-800 text-white, Not active: text-white/50 --> */}
-                            <a class="bg-gray-800 text-white p-4 inline-flex justify-center rounded-md" href="#">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 sm:h-6 sm:w-6" viewBox="0 0 20 20" fill="currentColor">
-                                    <path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z" />
-                                </svg>
-                            </a>
-                            <a class="text-white/50 p-4 inline-flex justify-center rounded-md hover:bg-gray-800 hover:text-white smooth-hover" href="#">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 sm:h-6 sm:w-6" viewBox="0 0 20 20" fill="currentColor">
-                                    <path fill-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd" />
-                                </svg>
-                            </a>
-                        </nav>
-                        <div class="flex items-center flex-row space-x-2 lg:space-x-0 lg:flex-col lg:space-y-2">
-                            <a class="text-white/50 p-4 inline-flex justify-center rounded-md hover:bg-gray-800 hover:text-white smooth-hover" href="#">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 sm:h-6 sm:w-6" viewBox="0 0 20 20" fill="currentColor">
-                                    <path fill-rule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd" />
-                                </svg>
-                            </a>
-                            <a class="text-white/50 p-4 inline-flex justify-center rounded-md hover:bg-gray-800 hover:text-white smooth-hover" href="#">
+
+                            <a onClick={handleLogout} class="text-white/50 p-4 inline-flex justify-center rounded-md hover:bg-gray-800 hover:text-white smooth-hover" href="#">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 sm:h-6 sm:w-6" viewBox="0 0 20 20" fill="currentColor">
                                     <path fill-rule="evenodd" d="M3 3a1 1 0 011 1v12a1 1 0 11-2 0V4a1 1 0 011-1zm7.707 3.293a1 1 0 010 1.414L9.414 9H17a1 1 0 110 2H9.414l1.293 1.293a1 1 0 01-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0z" clip-rule="evenodd" />
                                 </svg>
                             </a>
-                        </div>
+                        </nav>
                     </div>
                     {/* <!-- Content --> */}
                     <div class="flex-1 px-2 sm:px-0">
@@ -76,13 +72,6 @@ const Dashboard = () => {
                                             setSearch(e.target.value)
                                         }}
                                         name="search" placeholder="Search" />
-                                    <div class="relative">
-                                        <select class="border-2 border-gray-300 bg-white h-10 px-5 rounded-lg text-sm focus:outline-none">
-                                            <option value="">Filter</option>
-                                            <option value="lowToHigh">Price Low to High</option>
-                                            <option value="highToLow">Price High to Low</option>
-                                        </select>
-                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -95,13 +84,7 @@ const Dashboard = () => {
                                 </a></Link>
                                 <a class="text-white/50 group-hover:text-white group-hover:smooth-hover text-center" href="#">Add New</a>
                             </div>
-                            {/* <div class="relative group bg-gray-900 py-10 sm:py-20 px-4 flex flex-col space-y-2 items-center cursor-pointer rounded-md hover:bg-gray-900/80 hover:smooth-hover">
-                                <img class="w-20 h-20 object-cover object-center rounded-full" src="https://images.unsplash.com/photo-1547592180-85f173990554?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80" alt="cuisine" />
-                                <h4 class="text-white text-2xl font-bold capitalize text-center">Cuisine</h4>
-                                <p class="text-white/50">Price:</p>
-                                <p class="text-white/50">Quanitity:</p>
-                                <p class="absolute top-2 text-white/20 inline-flex items-center text-xs">View <span class="ml-2 w-2 h-2 block bg-green-500 rounded-full group-hover:animate-pulse"></span></p>
-                            </div> */}
+
                             {filteredVehicles.map((vehicle, index) => (
                                 <div
                                     key={index}
